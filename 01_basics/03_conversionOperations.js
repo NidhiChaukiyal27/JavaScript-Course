2b5ce498cd26d1a6b95809b025cb5dc5022aa400
NidhiChaukiyal27 <nidhichaukiyal789@gmail.com>
2025-01-29 06:15:38 +0000

    Conversions

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -30,12 +30,56 @@
 //"nidhi" => true
 
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);  //33
+// console.log(typeof stringNumber); //string 
 
 
+
+//*******************Operations *********************/
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue); // -3
+
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**2);
+//console.log(2/3);
+//console.log(2%2);
+
+// let str1 = "hello"
+// let str2 = " Nidhi"
+
+// let str3 = str1 + str2
+// console.log(str3); // hello Nidhi
+
+
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); //122
+// console.log(1 + 2 + "2") //32
+
+// // console.log((3 + 4) * 5 % 3);
+// console.log(+true); // 1
+// console.log(+""); //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+let gameCounter = 100 //102
+gameCounter++;
+console.log(gameCounter); //// 101
+++gameCounter;
+console.log(gameCounter);//102
+
+
+//read about prefix and postfix
 
 
 
