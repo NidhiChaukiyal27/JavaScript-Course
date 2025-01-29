56b18c6b05104de1a4ba19f878aa452dd60511c5
NidhiChaukiyal27 <nidhichaukiyal789@gmail.com>
2025-01-29 05:25:00 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,42 @@
+//let score = 33 ---> number
+//let score = "33" -----> string
+//let score = "33abc" -->type -> string, value--> NaN 
+//let score = null --->0
+//let score = undefined--->NaN
+//let score = true--->1
+//let score = false---->0
+
+
+
+// let score = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn); //// false
+
+
+//1 => true
+//0 => false
+//" " => false
+//"nidhi" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+
+
+
