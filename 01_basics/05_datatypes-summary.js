1797378782ee922e734390a23ff7c17db517f2a2
NidhiChaukiyal27 <nidhichaukiyal789@gmail.com>
2025-01-29 08:24:50 +0000

    datatype summary

---
+++ 01_basics/05_datatypes-summary.js
@@ -0,0 +1,51 @@
+//datatypes are of 2 types - primitive and non primitive
+//how data is storing in memory
+//primitive 
+// //7 types : string, number, boolean, null , undefined symbol, bigint
+
+//js is a dynamically typed language or statically typed language?
+const score =  100
+const scoreValue =  100.3
+
+const isLoggedIn =  false
+const outsideTemp  = null
+let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+// console.log(id === anotherId); //false
+
+const bigNumber = 3456782523748764774657585n
+
+
+
+
+
+
+//reference (Non Primitive)
+//Array, Objects , functions
+
+const heros = ["shaktiman", "naagraj", "doga"]; //array
+let myObj = {
+    name: "nidhi",//object
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+// console.log(typeof bigNumber); //bigint
+// console.log(typeof scoreValue); //number
+// console.log(typeof isLoggedIn); //boolean
+// console.log(typeof outsideTemp); //object
+
+// console.log(typeof myFunction); //function
+// console.log(typeof heros); //object
+
+// console.log(typeof anotherId); //symbol
+
+
+
+
