67a02f82e4275a9160f73885b3c938e7eb6c65a5
NidhiChaukiyal27 <nidhichaukiyal789@gmail.com>
2025-01-30 04:17:44 +0000

    date

---
+++ 01_basics/08_dates.js
@@ -0,0 +1,17 @@
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString);
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// console.log(typeof myDate); //object
+
+// let myCreatedDate = new Date(2025, 0, 30 , 5, 3)
+let myCreatedDate = new Date("01-14-2025")
+console.log(myCreatedDate.toLocaleString());
+
+
+
+/*********video no - 13 WATCH AGAIN */
+
+
